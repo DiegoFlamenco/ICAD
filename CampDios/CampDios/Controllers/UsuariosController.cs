c0c328707d877d96aceb636006cd54d54fd9c29e
MasterFernando <ferjhs14@gmail.com>
2017-12-13 17:52:52 -0600

    Creacion de registro de usuarios, controlador, vistas

---
+++ CampDios/CampDios/Controllers/UsuariosController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CampDios.Modelos;
+
+namespace CampDios.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private CampDiosEntities db = new CampDiosEntities();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.Miembro_Id = new SelectList(db.Miembros, "IdMiembro", "Nombres");
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Usuario_Id,Login,Contraseña,Fecha_Creacion,Miembro_Id")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios.Add(usuarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Miembro_Id = new SelectList(db.Miembros, "IdMiembro", "Nombres", usuarios.Miembro_Id);
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Miembro_Id = new SelectList(db.Miembros, "IdMiembro", "Nombres");
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Usuario_Id,Login,Contraseña,Fecha_Creacion,Miembro_Id")] Usuarios usuarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Miembro_Id = new SelectList(db.Miembros, "IdMiembro", "Nombres", usuarios.Miembro_Id);
+            return View(usuarios);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuarios usuarios = db.Usuarios.Find(id);
+            db.Usuarios.Remove(usuarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
