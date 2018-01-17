bcad1377a78885919bb7028dfb4e6845b228d766
Diego Hernandez <diego.jhf@gmail.com>
2018-01-16 22:30:45 -0600

    creacion mantenimiento tablas usuarios_opciones y opciones y agregar logout al menu

---
+++ CampDios/CampDios/Controllers/OpcionesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CampDios.Modelos;
+
+namespace CampDios.Controllers
+{
+    public class OpcionesController : Controller
+    {
+        private CampDiosEntities db = new CampDiosEntities();
+
+        // GET: Opciones
+        public ActionResult Index()
+        {
+            var opciones = db.Opciones.Include(o => o.Modulos);
+            return View(opciones.ToList());
+        }
+
+        // GET: Opciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opciones opciones = db.Opciones.Find(id);
+            if (opciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opciones);
+        }
+
+        // GET: Opciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.Modulos_Modulos_Id = new SelectList(db.Modulos, "Modulos_Id", "Nombre_Modulos");
+            return View();
+        }
+
+        // POST: Opciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Opciones_Id,Nombre_Opciones,Modulos_Modulos_Id")] Opciones opciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Opciones.Add(opciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Modulos_Modulos_Id = new SelectList(db.Modulos, "Modulos_Id", "Nombre_Modulos", opciones.Modulos_Modulos_Id);
+            return View(opciones);
+        }
+
+        // GET: Opciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opciones opciones = db.Opciones.Find(id);
+            if (opciones == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Modulos_Modulos_Id = new SelectList(db.Modulos, "Modulos_Id", "Nombre_Modulos", opciones.Modulos_Modulos_Id);
+            return View(opciones);
+        }
+
+        // POST: Opciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Opciones_Id,Nombre_Opciones,Modulos_Modulos_Id")] Opciones opciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(opciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Modulos_Modulos_Id = new SelectList(db.Modulos, "Modulos_Id", "Nombre_Modulos", opciones.Modulos_Modulos_Id);
+            return View(opciones);
+        }
+
+        // GET: Opciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Opciones opciones = db.Opciones.Find(id);
+            if (opciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(opciones);
+        }
+
+        // POST: Opciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Opciones opciones = db.Opciones.Find(id);
+            db.Opciones.Remove(opciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CampDios/CampDios/Controllers/Usuarios_OpcionesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CampDios.Modelos;
+
+namespace CampDios.Controllers
+{
+    public class Usuarios_OpcionesController : Controller
+    {
+        private CampDiosEntities db = new CampDiosEntities();
+
+        // GET: Usuarios_Opciones
+        public ActionResult Index()
+        {
+            var usuarios_Opciones = db.Usuarios_Opciones.Include(u => u.Opciones).Include(u => u.Usuarios);
+            return View(usuarios_Opciones.ToList());
+        }
+
+        // GET: Usuarios_Opciones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios_Opciones usuarios_Opciones = db.Usuarios_Opciones.Find(id);
+            if (usuarios_Opciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios_Opciones);
+        }
+
+        // GET: Usuarios_Opciones/Create
+        public ActionResult Create()
+        {
+            ViewBag.Opciones_Opciones_Id = new SelectList(db.Opciones, "Opciones_Id", "Nombre_Opciones");
+            ViewBag.Usuarios_Usuario_Id = new SelectList(db.Usuarios, "Usuarios_id", "Login");
+            return View();
+        }
+
+        // POST: Usuarios_Opciones/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idUsuario_Opciones,Usuarios_Usuario_Id,Opciones_Opciones_Id,Lectura,Escritura,Edicion")] Usuarios_Opciones usuarios_Opciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Usuarios_Opciones.Add(usuarios_Opciones);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Opciones_Opciones_Id = new SelectList(db.Opciones, "Opciones_Id", "Nombre_Opciones", usuarios_Opciones.Opciones_Opciones_Id);
+            ViewBag.Usuarios_Usuario_Id = new SelectList(db.Usuarios, "Usuarios_id", "Login", usuarios_Opciones.Usuarios_Usuario_Id);
+            return View(usuarios_Opciones);
+        }
+
+        // GET: Usuarios_Opciones/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios_Opciones usuarios_Opciones = db.Usuarios_Opciones.Find(id);
+            if (usuarios_Opciones == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Opciones_Opciones_Id = new SelectList(db.Opciones, "Opciones_Id", "Nombre_Opciones", usuarios_Opciones.Opciones_Opciones_Id);
+            ViewBag.Usuarios_Usuario_Id = new SelectList(db.Usuarios, "Usuarios_id", "Login", usuarios_Opciones.Usuarios_Usuario_Id);
+            return View(usuarios_Opciones);
+        }
+
+        // POST: Usuarios_Opciones/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idUsuario_Opciones,Usuarios_Usuario_Id,Opciones_Opciones_Id,Lectura,Escritura,Edicion")] Usuarios_Opciones usuarios_Opciones)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuarios_Opciones).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Opciones_Opciones_Id = new SelectList(db.Opciones, "Opciones_Id", "Nombre_Opciones", usuarios_Opciones.Opciones_Opciones_Id);
+            ViewBag.Usuarios_Usuario_Id = new SelectList(db.Usuarios, "Usuarios_id", "Login", usuarios_Opciones.Usuarios_Usuario_Id);
+            return View(usuarios_Opciones);
+        }
+
+        // GET: Usuarios_Opciones/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios_Opciones usuarios_Opciones = db.Usuarios_Opciones.Find(id);
+            if (usuarios_Opciones == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios_Opciones);
+        }
+
+        // POST: Usuarios_Opciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Usuarios_Opciones usuarios_Opciones = db.Usuarios_Opciones.Find(id);
+            db.Usuarios_Opciones.Remove(usuarios_Opciones);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
